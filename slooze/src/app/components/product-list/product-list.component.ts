8698e1a3242f2ff62640687f726847bcbec9aae3
chanankumar <chanankumar.ck@gmail.com>
2025-06-06 00:05:47 +0530

    group by feature

--- slooze/src/app/components/product-list/product-list.component.ts
+++ slooze/src/app/components/product-list/product-list.component.ts
@@ -13,17 +13,44 @@ export class ProductListComponent {
 
 
   products:any = null;
+  groupByCategory: string = 'none';
+  groupedProducts: { [key: string]: any[] } = {};
+  categoryVisibility: { [key: string]: boolean } = {};
+  
 
   constructor(private dataService: DataService, private router:Router, private apiService:ApiService, private route: ActivatedRoute) {
-    this.getProducts();
+    this.getProducts().then(this.groupProducts.bind(this));
   }
 
   async getProducts() {
-    this.products = await this.apiService.getProducts(20);
+    return new Promise(async (resolve, reject) => {
+      this.products = await this.apiService.getProducts(20);
+      resolve('got products');
+    })
   }
     
   editProduct(product: any) {
    this.dataService.setData(product);
    this.router.navigate(['/home/editproduct']);
   }
+
+  groupProducts() {
+    if (this.groupByCategory === 'category') {
+      this.groupedProducts = this.products?.reduce((acc:any, product:any) => {
+        const key = product.product_category || 'Uncategorized';
+        acc[key] = acc[key] || [];
+        acc[key].push(product);
+        return acc;
+      }, {} as { [key: string]: any[] }) || {};
+    } else {
+      this.groupedProducts = { All: this.products || [] };
+    }
+  }
+
+  toggleCategory(category: string) {
+    this.categoryVisibility[category] = !this.categoryVisibility[category];
+  }
+
+  objectKeys = Object.keys;
+
 }
